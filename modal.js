afb29df2b6929d61f4c317f87540939a9ffc30fc
orzaca <140673379+orzaca@users.noreply.github.com>
2024-05-09 21:40:39 -0500

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,156 @@
+/* REALIZA LA FUNCION DEL MODAL SALDOS*/
+
+function openTipi() {
+    document.getElementById('myModal').style.display = "block";
+}
+
+function closeTipi() {
+    document.getElementById('myModal').style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModal')) {
+        closeTipi();
+    }
+}
+
+function openGuion() {
+    document.getElementById('ModalGuion').style.display = "block";
+}
+
+function closeGuion() {
+    document.getElementById('ModalGuion').style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModal')) {
+        closeGuion();
+    }
+}
+
+/* REALIZA LA FUNCION DEL MODAL PAGOS */
+
+function openTipiPagos() {
+    document.getElementById('myModalPagos').style.display = "block";
+}
+
+function closeTipiPagos() {
+    document.getElementById('myModalPagos').style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModalPagos')) {
+        closeTipiPagos();
+    }
+}
+
+function openGuionPagos() {
+    document.getElementById('ModalGuionPagos').style.display = "block";
+}
+
+function closeGuionPagos() {
+    document.getElementById('ModalGuionPagos').style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModalPagos')) {
+        closeGuionPagos();
+    }
+}
+/* REALIZA LA FUNCION DEL MODAL ORDENES  */
+
+function openTipiOrden() {
+    document.getElementById('myModalOrden').style.display = "block";
+}
+
+function closeTipiOrden() {
+    document.getElementById('myModalOrden').style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModalOrden')) {
+        closeTipiOrden();
+    }
+}
+
+function openGuionOrden() {
+    document.getElementById('ModalGuionOrden').style.display = "block";
+}
+
+function closeGuionOrden() {
+    document.getElementById('ModalGuionOrden').style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModalOrden')) {
+        closeGuionOrden();
+    }
+}
+
+
+/* REALIZA LA FUNCION DEL MODAL PAGOS */
+
+function openTipiPagos() {
+    document.getElementById('myModalPagos').style.display = "block";
+}
+
+function closeTipiPagos() {
+    document.getElementById('myModalPagos').style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModalPagos')) {
+        closeTipiPagos();
+    }
+}
+
+function openGuionPagos() {
+    document.getElementById('ModalGuionPagos').style.display = "block";
+}
+
+function closeGuionPagos() {
+    document.getElementById('ModalGuionPagos').style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModalPagos')) {
+        closeGuionPagos();
+    }
+}
+/* REALIZA LA FUNCION DEL MODAL QUEJAS  */
+
+function openTipiQuejas() {
+    document.getElementById('myModalQuejas').style.display = "block";
+}
+
+function closeTipiQuejas() {
+    document.getElementById('myModalQuejas').style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModalQuejas')) {
+        closeTipiQuejas();
+    }
+}
+
+function openGuionQuejas() {
+    document.getElementById('ModalGuionQuejas').style.display = "block";
+}
+
+function closeGuionQuejas() {
+    document.getElementById('ModalGuionQuejas').style.display = "none";
+}
+
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById('myModalQuejas')) {
+        closeGuionQuejas();
+    }
+}
+
+
+
+
